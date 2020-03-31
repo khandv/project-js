a9b4274d7859bc619539963e22697ad926cbe37d
khan <khandv@gmail.com>
2020-03-31 21:08:45 +0300

    HW-7

---
+++ temp.js
@@ -0,0 +1,24 @@
+let x = 5; 
+console.log(++x);
+
+console.log([ ] + false - null + true);
+
+// let y = 1; 
+// let x = y = 2; 
+// console.log(x);
+
+console.log([ ] + 1 + 2);
+
+console.log("1"[0]);
+
+console.log(2 && 1 && null && 0 && undefined);
+
+console.log(!!( a && b ));
+
+console.log(null || 2 && 3 || 4);
+
+console.log(+"Infinity");
+
+console.log("Ёжик" > "яблоко");
+
+console.log(0 || "" || 2 || undefined || true || falsе);
