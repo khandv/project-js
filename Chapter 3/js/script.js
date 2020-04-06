d723b37bed4cbb1aa146c2b161bcece92cb90b35
khan <khandv@gmail.com>
2020-04-06 19:45:38 +0300

    HW-13

--- Chapter 3/js/script.js
+++ Chapter 3/js/script.js
@@ -118,4 +118,69 @@ window.addEventListener('DOMContentLoaded', function() {
         descrBtn.classList.remove('more-splash');
         document.body.style.overflow = '';
     });
+
+    // class options {
+    //     constructor(height, width, bg, fontSize, textAlign) {
+    //         this.height = height;
+    //         this.width = width;
+    //         this.bg = bg;
+    //         this.fontSize = fontSize;
+    //         this.textAlign = textAlign;
+    //     }
+    //     createDiv(height, width, bg, fontSize, textAlign) {
+    //         let elemDiv = document.createElement('div');
+    //         elemDiv.textContent = textAlign;
+    //         elemDiv.style.height = height;
+    //         elemDiv.style.width = width;
+    //         elemDiv.style.background = bg;
+    //         elemDiv.style.fontSize = fontSize;
+    //         document.body.appendChild(elemDiv);
+    //     }
+    // }
+
+    // Form
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся!',
+        failure: 'Что-то пошло не так...'
+    };
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+        
+    statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+        let formData = new FormData(form);
+
+        let obj = {};
+        formData.forEach(function(value, key) {
+            obj[key] = value;
+        });
+        let json = JSON.stringify(obj);
+
+        request.send(json);
+
+        request.addEventListener('readystatechange', function() {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+    });
+
 });
