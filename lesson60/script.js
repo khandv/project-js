82b6a51d7162e6d82be8e9caa1aa6e7c9e5ba27d
khan <khandv@gmail.com>
2020-04-17 17:46:42 +0300

    lesson60

---
+++ lesson60/script.js
@@ -0,0 +1,29 @@
+class GotService {
+    constructor() {
+        this._apiBase = 'https://www.anapioficeandfire.com/api'
+    } 
+    async getResourse(url) {
+        const res = await fetch(`${this._apiBase}${url}`);
+    
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }    
+        return await res.json();
+    };
+    getAllCharacters() {
+        return this.getResourse('/characters?page=5&pageSize=10');
+    }
+    getCharacter(id) {
+        return this.getResourse(`/characters/${id}`);
+    }
+}
+
+const got = new GotService();
+
+got.getAllCharacters()
+    .then(res => {
+        console.log(res.forEach(item => console.log(item.name)))
+    });
+
+got.getCharacter(130)
+    .then(res => console.log(res));
